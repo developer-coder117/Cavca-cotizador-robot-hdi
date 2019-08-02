59d3e16d775faf4a9b103d3646233f416dab22b1
developer-coder117 <jsoler@dbsolution.com.co>
2019-08-02 18:09:13 -0500

    Bot seguros del estado

--- dist/controller/SegurosEstado.js
+++ dist/controller/SegurosEstado.js
@@ -43,6 +43,69 @@ class SEBot {
                 yield page.type(config_json_1.default.SegurosEstado.login.password, config_json_1.default.SegurosEstado.credentials.pass);
                 yield page.waitForSelector(config_json_1.default.SegurosEstado.login.btn);
                 yield page.click(config_json_1.default.SegurosEstado.login.btn);
+                yield Promise.all([
+                    page.goto(config_json_1.default.SegurosEstado.formParams.formOptions),
+                    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                ]);
+                yield pendingXHR.waitForAllXhrFinished();
+                yield Promise.all([
+                    page.goto(config_json_1.default.SegurosEstado.formParams.frame),
+                    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                ]);
+                yield page.click(config_json_1.default.SegurosEstado.formParams.option);
+                yield pendingXHR.waitForAllXhrFinished();
+                yield Promise.all([
+                    yield page.click(config_json_1.default.SegurosEstado.formParams.newForm),
+                    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                ]);
+                //formulario1
+                yield pendingXHR.waitForAllXhrFinished();
+                //Parte 1
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.selectIdDoc);
+                yield page.select(config_json_1.default.SegurosEstado.formulario1.selectIdDoc, config_json_1.default.SegurosEstado.formulario1.idDoc);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.fieldNumId);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.fieldNumId, config_json_1.default.SegurosEstado.formulario1.valNumId);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.Nom);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.Nom, config_json_1.default.SegurosEstado.formulario1.NomVal);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.Apellido);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.Apellido, config_json_1.default.SegurosEstado.formulario1.valApellido);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.selectGen);
+                yield page.select(config_json_1.default.SegurosEstado.formulario1.selectGen, config_json_1.default.SegurosEstado.formulario1.Genero);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.bd);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.bd, config_json_1.default.SegurosEstado.formulario1.bdval);
+                yield page.select(config_json_1.default.SegurosEstado.formulario1.estadoCivil, config_json_1.default.SegurosEstado.formulario1.estadoCival);
+                yield page.select(config_json_1.default.SegurosEstado.formulario1.trabajo, config_json_1.default.SegurosEstado.formulario1.trabajoIn);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.emailField);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.emailField, config_json_1.default.SegurosEstado.formulario1.emailAddress);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.tel);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.tel, config_json_1.default.SegurosEstado.formulario1.telNumber);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.direccion);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.direccion, config_json_1.default.SegurosEstado.formulario1.direccionNumber);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.direccion);
+                yield page.type(config_json_1.default.SegurosEstado.formulario1.direccion, config_json_1.default.SegurosEstado.formulario1.direccionNumber);
+                //Parte 2 
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario1.secondpart);
+                yield page.click(config_json_1.default.SegurosEstado.formulario1.secondpart);
+                yield pendingXHR.waitForAllXhrFinished();
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.selectProducto);
+                yield page.select(config_json_1.default.SegurosEstado.formulario2.selectProducto, config_json_1.default.SegurosEstado.formulario2.valueProducto);
+                yield pendingXHR.waitForAllXhrFinished();
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.zonaCirculacion);
+                yield page.evaluate((conf) => {
+                    document.querySelector(conf.formulario2.zonaCirculacion).value = conf.formulario2.zonaValue;
+                }, config_json_1.default.SegurosEstado);
+                yield pendingXHR.waitForAllXhrFinished();
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.marca);
+                yield page.type(config_json_1.default.SegurosEstado.formulario2.marca, config_json_1.default.SegurosEstado.formulario2.marcaValue);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.clase);
+                yield page.type(config_json_1.default.SegurosEstado.formulario2.clase, config_json_1.default.SegurosEstado.formulario2.claseValue);
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.placa);
+                yield page.type(config_json_1.default.SegurosEstado.formulario2.placa, config_json_1.default.SegurosEstado.formulario2.placaIn);
+                yield pendingXHR.waitForAllXhrFinished();
+                //Parte 3 
+                yield page.waitForSelector(config_json_1.default.SegurosEstado.formulario2.thirdPart);
+                yield page.click(config_json_1.default.SegurosEstado.formulario2.thirdPart);
+                yield pendingXHR.waitForAllXhrFinished();
             }
             catch (error) {
                 console.log("Error");
--- src/controller/SegurosEstado.ts
+++ src/controller/SegurosEstado.ts
@@ -32,6 +32,67 @@ export class SEBot  implements GlobalFunction {
             await page.type(data.SegurosEstado.login.password,data.SegurosEstado.credentials.pass)
             await page.waitForSelector(data.SegurosEstado.login.btn)
             await page.click(data.SegurosEstado.login.btn)
+            await Promise.all([
+                page.goto(data.SegurosEstado.formParams.formOptions),
+                page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                    ]);
+            await pendingXHR.waitForAllXhrFinished()
+            await Promise.all([
+                page.goto(data.SegurosEstado.formParams.frame),
+                page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                    ]);
+            await page.click(data.SegurosEstado.formParams.option)
+            await pendingXHR.waitForAllXhrFinished()
+            await Promise.all([
+            await page.click(data.SegurosEstado.formParams.newForm),
+            page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                 ]);
+            //formulario1
+            await pendingXHR.waitForAllXhrFinished()
+            //Parte 1
+            await page.waitForSelector(data.SegurosEstado.formulario1.selectIdDoc)
+            await page.select(data.SegurosEstado.formulario1.selectIdDoc,data.SegurosEstado.formulario1.idDoc)
+            await page.waitForSelector(data.SegurosEstado.formulario1.fieldNumId)
+            await page.type(data.SegurosEstado.formulario1.fieldNumId,data.SegurosEstado.formulario1.valNumId)
+            await page.waitForSelector(data.SegurosEstado.formulario1.Nom);
+            await page.type(data.SegurosEstado.formulario1.Nom,data.SegurosEstado.formulario1.NomVal);
+            await page.waitForSelector(data.SegurosEstado.formulario1.Apellido);
+            await page.type(data.SegurosEstado.formulario1.Apellido,data.SegurosEstado.formulario1.valApellido);
+            await page.waitForSelector(data.SegurosEstado.formulario1.selectGen);
+            await page.select(data.SegurosEstado.formulario1.selectGen,data.SegurosEstado.formulario1.Genero);
+            await page.waitForSelector(data.SegurosEstado.formulario1.bd);
+            await page.type(data.SegurosEstado.formulario1.bd,data.SegurosEstado.formulario1.bdval)
+            await page.select(data.SegurosEstado.formulario1.estadoCivil,data.SegurosEstado.formulario1.estadoCival)
+            await page.select(data.SegurosEstado.formulario1.trabajo,data.SegurosEstado.formulario1.trabajoIn)
+            await page.waitForSelector(data.SegurosEstado.formulario1.emailField)
+            await page.type(data.SegurosEstado.formulario1.emailField,data.SegurosEstado.formulario1.emailAddress)
+            await page.waitForSelector(data.SegurosEstado.formulario1.tel)
+            await page.type(data.SegurosEstado.formulario1.tel,data.SegurosEstado.formulario1.telNumber)
+            await page.waitForSelector(data.SegurosEstado.formulario1.direccion)
+            await page.type(data.SegurosEstado.formulario1.direccion,data.SegurosEstado.formulario1.direccionNumber)
+            await page.waitForSelector(data.SegurosEstado.formulario1.direccion)
+            await page.type(data.SegurosEstado.formulario1.direccion,data.SegurosEstado.formulario1.direccionNumber)
+            //Parte 2 
+            await page.waitForSelector(data.SegurosEstado.formulario1.secondpart)
+            await page.click(data.SegurosEstado.formulario1.secondpart)
+            await pendingXHR.waitForAllXhrFinished()
+            await page.waitForSelector(data.SegurosEstado.formulario2.selectProducto)
+            await page.select(data.SegurosEstado.formulario2.selectProducto,data.SegurosEstado.formulario2.valueProducto)
+            await pendingXHR.waitForAllXhrFinished()
+            await page.waitForSelector(data.SegurosEstado.formulario2.zonaCirculacion)
+            await page.select(data.SegurosEstado.formulario2.zonaCirculacion,data.SegurosEstado.formulario2.zonaValue)
+            await pendingXHR.waitForAllXhrFinished()
+            await page.waitForSelector(data.SegurosEstado.formulario2.marca)
+            await page.type(data.SegurosEstado.formulario2.marca,data.SegurosEstado.formulario2.marcaValue)
+            await page.waitForSelector(data.SegurosEstado.formulario2.clase)
+            await page.type(data.SegurosEstado.formulario2.clase,data.SegurosEstado.formulario2.claseValue)
+            await page.waitForSelector(data.SegurosEstado.formulario2.placa)
+            await page.type(data.SegurosEstado.formulario2.placa,data.SegurosEstado.formulario2.placaIn)
+            await pendingXHR.waitForAllXhrFinished()
+            //Parte 3 
+            await page.waitForSelector(data.SegurosEstado.formulario2.thirdPart)
+            await page.click(data.SegurosEstado.formulario2.thirdPart)
+            await pendingXHR.waitForAllXhrFinished()
             }  catch (error) {
             console.log("Error")
             await page.waitFor(100000)
